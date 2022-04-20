d8319c2f33f1b3b7b49789ff75b0473fed2c829f
admin <kalem@enterpriseautomation.co.uk>
2022-04-20 09:07:12 +0100

    init

---
+++ src/components/HeroSection.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <h1>HOME</h1>
+      <p>EAS Quiz Bot Stats</p>
+      <div className='hero-btns'>
+        <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
+          Sign In
+        </Button>
+        <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large' onClick={console.log('hey')}>
+          Sign Up <i className='far fa-play-circle' />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
+
